04024a7073c4e75eaf59abcbc9bf2d5a0631cf72
oceenachi <phillipfrancisca@gmail.com>
2020-03-14 20:00:42 +0100

    add user controller class

---
+++ src/main/java/com/statuspage/status/controller/WebsiteController.java
@@ -0,0 +1,46 @@
+package com.statuspage.status.controller;
+
+import com.statuspage.status.dto.request.WebsitePayload;
+import com.statuspage.status.dto.response.CreateResponse;
+import com.statuspage.status.model.Website;
+import com.statuspage.status.service.AllServices;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/v1")
+public class WebsiteController {
+
+    private ModelMapper modelMapper;
+    private AllServices allServices;
+
+    @Autowired
+    public WebsiteController(ModelMapper modelMapper, AllServices allServices){
+        this.modelMapper = modelMapper;
+        this.allServices = allServices;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<CreateResponse> createWebsite(@RequestBody WebsitePayload websiteInfo){
+        Website website = modelMapper.map(websiteInfo, Website.class);
+        CreateResponse createResponse = allServices.createWebsite(website);
+        return new ResponseEntity<>(createResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/websites")
+    public List<Website> getAllWebsites(){
+//        System.out.println(allServices.getAllWebsites());
+        return allServices.getAllWebsites();
+    }
+
+
+
+
+}
