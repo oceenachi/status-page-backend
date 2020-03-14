345a894e6b1336aec091254f37f7d1a6e943fc75
oceenachi <phillipfrancisca@gmail.com>
2020-03-14 20:05:18 +0100

    configure BcryptPasswordEncoder, model mapper and rest template beans

---
+++ src/main/java/com/statuspage/status/StatusApplication.java
@@ -0,0 +1,47 @@
+package com.statuspage.status;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+
+@EnableScheduling
+@SpringBootApplication
+public class StatusApplication {
+
+    @Bean
+    BCryptPasswordEncoder BCryptPasswordEncoder(){return new BCryptPasswordEncoder();}
+
+
+    @Bean
+    ModelMapper ModelMapper(){
+        return new ModelMapper();
+    }
+
+    @Bean
+    RestTemplate RestTemplate(RestTemplateBuilder builder){
+        return builder.build();
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatusApplication.class, args);
+    }
+
+
+}
