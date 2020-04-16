b28473ffe256ea4c77b5b24941a5100db1ba53ac
oceenachi <phillipfrancisca@gmail.com>
2020-04-16 20:27:10 +0100

    Add functionality for response success and failure

--- src/main/java/com/statuspage/status/service/AsyncServices.java
+++ src/main/java/com/statuspage/status/service/AsyncServices.java
@@ -1,19 +1,21 @@
 package com.statuspage.status.service;
 
+import com.statuspage.status.Repository.IncidentRepository;
+import com.statuspage.status.Repository.RequestRepository;
 import com.statuspage.status.Repository.WebsiteRepository;
-import com.statuspage.status.model.Request;
-import com.statuspage.status.model.Website;
+import com.statuspage.status.model.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
+import java.time.Instant;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.*;
 import java.util.stream.Collectors;
 
@@ -23,13 +25,17 @@ public class AsyncServices {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     WebsiteRepository websiteRepository;
-
-    @Autowired
+    RequestRepository requestRepository;
+    IncidentRepository incidentRepository;
     private RestTemplate restTemplate;
 
-    public AsyncServices(WebsiteRepository websiteRepository, RestTemplate restTemplate) {
+    @Autowired
+    public AsyncServices(WebsiteRepository websiteRepository, RestTemplate restTemplate,
+                         RequestRepository requestRepository, IncidentRepository incidentRepository) {
         this.websiteRepository = websiteRepository;
         this.restTemplate = restTemplate;
+        this.requestRepository = requestRepository;
+        this.incidentRepository = incidentRepository;
     }
 
 
@@ -37,54 +43,74 @@ public class AsyncServices {
         return restTemplate.getForEntity(url, String.class);
     }
 
+    public int processResponse(String url, int responseCode){
+        Request newRequest = new Request();
+        newRequest.setResponseCode(responseCode);
+        newRequest.setRequestTime(Instant.now());
+        newRequest.setWebsiteUrl(url);
+        requestRepository.save(newRequest);
 
-    @PostConstruct
+        System.out.println("i got to process response");
+        if(responseCode >= 300){
+            responseFailure(newRequest, url);
+        }else{
+            responseSuccess(url);
+        }
+        System.out.println( responseCode + url);
+        return responseCode;
+
+    }
+
+    public void responseSuccess( String url){
+        System.out.println("success");
+        websiteRepository.updateWebsiteStatus(StatusName.Operational, url);
+
+    }
+
+    public void responseFailure(Request newRequest, String url){
+        System.out.println("failure");
+        websiteRepository.updateWebsiteStatus(StatusName.Unserviceable, url);
+
+        Incident newIncident = new Incident();
+        newIncident.setIncidentStatus(IncidentStatus.Investigating);
+
+        Optional<Website> downWebsite = websiteRepository.findByUrl(url);
+        assert downWebsite.isPresent();
+
+        String newMessage = downWebsite.get().getName() + " is currently down for some users" + '\n' +
+                "Our engineering team is currently working hard to resolve this issue";
+        newIncident.setMessage(newMessage);
+        newIncident.setIncidentTime(Instant.now());
+        newIncident.setIsResolved(false);
+        newIncident.setRequest(newRequest);
+        incidentRepository.save(newIncident);
+
+    }
+
+    @Scheduled(cron = "* */5 * * * ?")
     private void makeAsyncCalls() throws ExecutionException, InterruptedException, TimeoutException {
         List<Website> websites = websiteRepository.findAll();
 //        list of urls
 //        [https://www.whogohost.ng/, https://www.nairaland.com/, https://stackoverflow.com/,
 //        https://bitly.com/, https://www.carmax.com/, https://www.betexplorer.com/soccer/australia/]
-        List<String> stringListOfWebsites = new ArrayList<>();
-        stringListOfWebsites.add("https://www.whogohost.ng/");
-        stringListOfWebsites.add("https://www.nairaland.com");
-        stringListOfWebsites.add("https://stackoverflow.com/");
-        stringListOfWebsites.add("https://bitly.com");
-        stringListOfWebsites.add("https://www.carmax.com/");
-        stringListOfWebsites.add("https://www.betexplorer.com/soccer/australia/");
-
 
         List<String> urls = websites.stream().map(Website::getUrl).collect(Collectors.toList());
         System.out.println(urls);
 
         for (String url : urls) {
-            Future<Integer> response = CompletableFuture.supplyAsync(() -> this.getResponses(url).getStatusCodeValue());
 
-            System.out.println(url + response.get(10000, TimeUnit.SECONDS));
-        }
+                CompletableFuture<Integer> response = CompletableFuture.supplyAsync(() -> {
+                    try{
+                        return this.getResponses(url).getStatusCodeValue();
+                    }catch (Exception ex){
+                        throw new IllegalArgumentException(ex.getMessage());
+                    }
+                }).thenApplyAsync(res-> processResponse(url, res))
 
-
-        for (String url : stringListOfWebsites) {
-
-            CompletableFuture<Integer> response = CompletableFuture.supplyAsync(() -> {
-                try {
-
-                    return getResponses(url).getStatusCodeValue();
-
-                } catch (Exception ex) {
-                    //throw your custom exception here
-                    throw new IllegalArgumentException(ex.getMessage());
-                }
-
-            }).thenApplyAsync(req -> req) //This {thenApplyAsync} block is in case you want to do another thing with the
-                    // response from the try block above, you can take it out if there's nothing to be done
-
-                    //you can return whatever error you want here, i just decided to return 1
-                    //This block is the reason your app was crashing you were not handling it.
-                    .exceptionally(exception -> {
-                        log.info(String.valueOf(exception));
-                        return 1;
-                    });
-
+                        .exceptionally(exception -> {
+                            log.info(String.valueOf(exception));
+                            return HttpStatus.SERVICE_UNAVAILABLE.value();
+                        }).thenApplyAsync(res-> processResponse(url, res));
             log.info("url {} {}", url, response.get(10000, TimeUnit.SECONDS));
         }
 
